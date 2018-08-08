98e4ad11180887c6a6e2f2c45d8db47fa05edba4
Glitch (bernardthompson-playgym-js) <none>
2018-08-08 15:30:07 +0000

    member and assessments loaded from json

---
+++ models/assessment-store.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const logger = require('../utils/logger');
+
+const memberCollection = require('./member-store.json').memberCollection;
+                       
+module.exports = memberCollection;                       
+                       
